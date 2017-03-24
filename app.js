143e490b176fa79332f81cc82220f93e9a416c6b
snippet-java <snippet.java@gmail.com>
2017-03-24 09:47:09 -0400

    update app.js

--- app.js
+++ app.js
@@ -4,4 +4,21 @@ var app = express();
 app.get('/', function(req, res) {
   res.send('Welcome to Node Bluemix');
 })
+var fs = require('fs');
+ 
+ 
+if (process.argv.length <= 2) {
+    console.log("Usage: " + __filename + " path/to/directory");
+    process.exit(-1);
+}
+ 
+var path = process.argv[2];
+ 
+fs.readdir(path, function(err, items) {
+    console.log(items);
+ 
+    for (var i=0; i<items.length; i++) {
+        console.log(items[i]);
+    }
+});
 app.listen(port);
